1220edc907e6ab2e9c48332554d8d3bcb6245c48
Ibis Fortunato <ibisfortunato@gmail.com>
2023-06-28 03:24:36 -0300

    Manejo de Excepciones

--- src/main/java/com/example/clase23/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/clase23/exception/ResourceNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.clase23.exception;public class ResourceNotFoundException {
+package com.example.clase23.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String mensaje){
+        super(mensaje);
+    }
 }
